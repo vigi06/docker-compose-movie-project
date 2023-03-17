069b04d0531c7455fbe78837168350b42b735f5f
Ubuntu <ubuntu@ip-172-31-51-138.ec2.internal>
2023-03-17 14:52:37 +0000

    added files

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
+    .connect('mongodb://52.87.220.24:27017/cinema', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
